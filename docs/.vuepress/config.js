03fa7e2dc28f9ea27bd008fb4873bce9c8201372
lkaien <lkaientan@gmail.com>
2019-11-12 07:56:57 +0800

    add page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: '前端记录',
     base:'/tech/',
-    description: 'welcome to my tech blog',
+    description: '欢迎来到我的博客',
     markdown:{
         lineNumbers: true
     },
